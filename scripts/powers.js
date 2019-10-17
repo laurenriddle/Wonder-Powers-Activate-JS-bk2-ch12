e438d50818fc5989b8b69249314c0b84924e2d59
Lauren Riddle <lari7132@gmail.com>
2019-10-17 09:22:10 -0500

    wrote the functions to put a class of enabled on the first three buttons when they are clicked

--- scripts/powers.js
+++ scripts/powers.js
@@ -1,1 +1,26 @@
-document.querySelector("#activate-flight").addEventListener()
+document.querySelector("#activate-flight").addEventListener("click", flightHandlerFunction)
+
+const flightSection = document.querySelector("#flight")
+
+function flightHandlerFunction () {
+    flightSection.classList.remove("disabled")
+    flightSection.classList.add("enabled")
+}
+
+
+document.querySelector("#activate-mindreading").addEventListener("click", mindReadingHandlerFunction)
+
+const mindReadingSection = document.querySelector("#mindreading")
+function mindReadingHandlerFunction () {
+    mindReadingSection.classList.remove("disabled")
+    mindReadingSection.classList.add("enabled")
+}
+
+
+document.querySelector("#activate-xray").addEventListener("click", xrayHandlerFunction)
+
+const xraySection = document.querySelector("#xray")
+function xrayHandlerFunction () {
+    xraySection.classList.remove("disabled")
+    xraySection.classList.add("enabled")
+}
